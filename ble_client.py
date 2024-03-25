3b8967666a4e1912a2e312969a9a15b146cfd5b0
anthony <anthonybruno63@yahoo.com>
2024-03-25 08:04:29 -0700

    storing device id in json file and python script to load from it

--- ble_client.py
+++ ble_client.py
@@ -2,11 +2,9 @@ import time
 import asyncio
 from bleak import BleakClient
 
-address = "34:85:18:7A:99:31"
-service = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 
-async def main(address):
+async def state(address):
     async with BleakClient(address) as client:
         while client:
             out = await client.read_gatt_char(service)
@@ -16,4 +14,6 @@ async def main(address):
                 print("Fine!")
             time.sleep(5)
 
-asyncio.run(main(address))
+
+
+
